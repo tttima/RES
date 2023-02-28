d5522925b7d191e437b302be4b6aac41e9676c92
tttima <126337965+tttima@users.noreply.github.com>
2023-02-28 23:22:33 +0600

    Delete style directory

--- style/app.js
+++
@@ -1,7 +0,0 @@
-const burger = aocument.querySelector('#burger');
-const menu = document.querySelector('#menu');
-
-burger.addEventListener('click', () => {
-    menu.classList.toggle('disp');
-
-});
